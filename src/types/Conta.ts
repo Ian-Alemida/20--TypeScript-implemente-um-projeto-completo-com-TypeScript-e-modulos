cc9fa1b7a0d564bc020a03775a07105cc26b5428
ian-alemida <iangtaonline1@gmail.com>
2024-12-14 14:07:11 -0300

    criando type resumo transações e logica no componente conta

--- src/types/Conta.ts
+++ src/types/Conta.ts
@@ -1,4 +1,5 @@
 import { GrupoTransacao } from "./GrupoTransacao.js";
+import { ResumoTransacoes } from "./ResumoTransacoes.js";
 import { TipoTransacao } from "./tipoTransacao.js";
 import { Transacao } from "./transacao.js";
 
@@ -71,6 +72,31 @@ const Conta = {
         transacoes.push(novaTransacao);
         console.log(this.getGruposTransacoes());
         localStorage.setItem("transacoes", JSON.stringify(transacoes));
+    },
+
+    agruparTransacoes(): ResumoTransacoes {
+        const resumo: ResumoTransacoes = { 
+        totalDepositos: 0, 
+        totalTransferencias: 0,         
+        totalPagamentosBoleto: 0 
+        };
+    
+        this.transacoes.forEach(transacao => {
+            switch (transacao.tipoTransacao) {
+                case TipoTransacao.DEPOSITO:
+                    resumo.totalDepositos += transacao.valor;
+                    break;
+    
+                case TipoTransacao.TRANSFERENCIA:
+                    resumo.totalTransferencias += transacao.valor;
+                    break;
+    
+                case TipoTransacao.PAGAMENTO_BOLETO:
+                    resumo.totalPagamentosBoleto += transacao.valor;
+                    break;
+            }
+        });
+        return resumo;
     }
 }
 
